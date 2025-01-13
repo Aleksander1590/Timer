281b7d82a7ba364ab3caeb821a80e455036ac1ce
Aleksander1590 <romanov1590@gmail.com>
2025-01-13 19:32:11 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -16,18 +16,22 @@ bot = telebot.TeleBot(TG_TOKEN)
 
 def render_progressbar(total, iteration, prefix='', suffix='', length=30, fill='█', zfill='░'):
     iteration = min(total, iteration)
-    percent = "{0:.1f}"
-    percent = percent.format(100 * (iteration / float(total)))
+    percent = "{0:.1f}".format(100 * (iteration / float(total)))
     filled_length = int(length * iteration // total)
     pbar = fill * filled_length + zfill * (length - filled_length)
-    return '{0} |{1}| {2}% {3}'.format(prefix, pbar, percent, suffix)
+    return f'{prefix} |{pbar}| {percent}% {suffix}'
 
 
 def notify_progress(chat_id, secs_left):
     progress_msg = bot.send_message(chat_id, f"Запуск таймера на {secs_left} секунд...")
+
     for remaining in range(secs_left, -1, -1):
         progress_bar = render_progressbar(secs_left, remaining)
-        bot.edit_message_text(f"Осталось {remaining} секунд {progress_bar}", chat_id, progress_msg.message_id)
+        bot.edit_message_text(
+            f"Осталось {remaining} секунд {progress_bar}",
+            chat_id,
+            progress_msg.message_id
+        )
         if remaining == 0:
             bot.send_message(chat_id, "Время вышло!")
         time.sleep(1)
@@ -40,6 +44,11 @@ def choose(message):
         notify_progress(message.chat.id, delay)
 
 
+@bot.message_handler(commands=['start'])
+def send_welcome(message):
+    bot.reply_to(message, "Бот запущен! На сколько зупустить таймер?")
+
+
 def main():
     @bot.message_handler(content_types=['text'])
     def handle_message(message):
@@ -49,4 +58,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-    
